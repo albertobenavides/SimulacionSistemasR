877be0f18ec17a527ee5a043dce3287c28e63816
Alberto Benavides <bena.87@hotmail.com>
2017-08-08 22:12:38 -0500

    Detección de retornos, paralelización y muestra en gráfica de resultados.

--- Practica1/main.R
+++ Practica1/main.R
@@ -3,30 +3,51 @@
 #Estudiar de forma sistemática y automatizada el tiempo de ejecución de una caminata en términos del largo de la caminata y la dimensión.
 #Realizar una comparación entre una implementación paralela y otra versión que no aproveche paralelismo.
 
-dimensions <- 1 # cantidad de dimensiones
-steps <- 10000 # cantidad de pasos
-position <- rep(0, dimensions)
-origin <- rep(0, dimensions)
-timesReturned <- 0
-for(step in 1:steps){
-  d <- sample(1:dimensions, 1)
-  if(runif(1) > 0.5){
-    position[d] <- position[d] + 1
-  } else{
-    position[d] <- position[d] - 1
-  }
+library(parallel)
 
-  if(all(position == origin)){
-    timesReturned <- timesReturned + 1
-  }
+dimensions <- 8
+repetitions <- 100
+steps <- 1000
 
-  distanceToOrigin <- sum(abs(point - origin))
-  stepsLeft <- steps - step
+output <- data.frame()
 
-  if(distanceToOrigin > stepsLeft){
-    print("No puede regresar con los pasos restantes")
-    break
+experiment <- function(r){
+  position <- rep(0, dimension)
+  origin <- rep(0, dimension)
+  timesReturned <- 0
+  for(step in 1:steps){
+    d <- sample(1:dimension, 1)
+    if(runif(1) > 0.5){
+      position[d] <- position[d] + 1
+    } else{
+      position[d] <- position[d] - 1
+    }
+
+    if(all(position == origin)){
+      timesReturned <- timesReturned + 1
+    }
+
+    distanceToOrigin <- sum(abs(position - origin))
+    stepsLeft <- steps - step
+
+    if(distanceToOrigin > stepsLeft){
+      break
+    }
   }
+  return(timesReturned)
 }
-print("Regresos")
-print(timesReturned)
+
+cluster <- makeCluster(detectCores() - 1)
+clusterExport(cluster, "steps")
+
+for (dimension in 1:dimensions) {
+  clusterExport(cluster, "dimension")
+  result <- parSapply(cluster, 1:repetitions, experiment)
+  output <- rbind(output, result)
+}
+stopCluster(cluster)
+
+png("p1.png")
+boxplot(data.matrix(output), use.cols=FALSE,
+xlab="Dimensi\u{F3}n", ylab="Retornos al origen", main="Retornos al origen por dimensi\u{F3}n")
+graphics.off()
