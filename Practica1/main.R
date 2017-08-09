96ae21b8392d091cb7519b1af6063d8c0b21f343
Alberto Benavides <bena.87@hotmail.com>
2017-08-08 21:19:36 -0500

    Agregados los objetivos como comentarios. -Simplificado el proyecto a un archivo; preparado para paralelismo

--- Practica1/main.R
+++ Practica1/main.R
@@ -1,6 +1,10 @@
-source("distance.R")
-dimensions <- 8 # cantidad de dimensiones
-steps <- 1000 # cantidad de pasos
+# Examina de manera sistemática los efectos de la dimensión en el número de veces que la caminata regresa al origen durante una caminata del movimiento Browniano.
+#Verificar que el número de pasos de la caminata o el número de repeticiones del experimento no estén causando un efecto significativo.
+#Estudiar de forma sistemática y automatizada el tiempo de ejecución de una caminata en términos del largo de la caminata y la dimensión.
+#Realizar una comparación entre una implementación paralela y otra versión que no aproveche paralelismo.
+
+dimensions <- 1 # cantidad de dimensiones
+steps <- 10000 # cantidad de pasos
 position <- rep(0, dimensions)
 origin <- rep(0, dimensions)
 timesReturned <- 0
@@ -11,15 +15,18 @@ for(step in 1:steps){
   } else{
     position[d] <- position[d] - 1
   }
-  print(position)
 
   if(all(position == origin)){
     timesReturned <- timesReturned + 1
   }
 
-  if(distanceToOrigin(position,"") > steps - step){
-    print("Ya no se puede regresar")
+  distanceToOrigin <- sum(abs(point - origin))
+  stepsLeft <- steps - step
+
+  if(distanceToOrigin > stepsLeft){
+    print("No puede regresar con los pasos restantes")
     break
   }
 }
+print("Regresos")
 print(timesReturned)
