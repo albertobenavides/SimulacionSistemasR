fef3fd598f6885cab3a6d695e830fbba85d54658
Alberto Benavides <bena.87@hotmail.com>
2017-09-14 11:01:00 -0500

    Práctica 6, Tarea 1 Terminada

--- p6/main.R
+++ p6/main.R
@@ -3,6 +3,8 @@ space <- 1
 totalAgents <- 50
 maxVelocity <- 1 / 20
 infectionProbability = 0.05
+recuperationProbability = 0.02
+infectionRadius <- 0.1
 maxTime <- 100
 
 agents <- data.frame(
@@ -24,8 +26,26 @@ agents <- data.frame(
 levels(agents$state) <- c("I", "S", "R")
 
 update <- function(){
-  # todo: agregar infección
   agent <- agents[i, ]
+  if(agent$state == "S"){
+    for (j in 1:totalAgents) {
+      infectedAgent <- agents[agents$state == "I", ]
+      d = sqrt(
+        (agent$x - infectedAgent$x) * (agent$x - infectedAgent$x) +
+        (agent$y - infectedAgent$y) * (agent$y - infectedAgent$y)
+      )
+      if(d < infectionRadius){
+        p <- (infectionRadius - d) / infectionRadius
+        if(runif(1) < p){
+          agent$state <- "I"
+        }
+      }
+    }
+  } else if(agent$state == "I"){
+    if(runif(1) < recuperationProbability){
+      agent$state <- "R"
+    }
+  }
   agent$x <- agent$x + agent$dx
   agent$y <- agent$y + agent$dy
   if(agent$x > space){
@@ -51,7 +71,18 @@ for (generation in 1:maxTime) {
 
   png(paste("img/", sprintf("%03d", generation), ".png", sep=""))
   plot(1, type="n", main = generation, xlim = c(0, space), ylim = c(0, space), xlab = "x", ylab = "y")
-  points(agents$x, agents$y, pch=15, col="chartreuse3", bg="chartreuse3")
+  aS <- agents[agents$state == "S", ]
+  aI <- agents[agents$state == "I", ]
+  aR <- agents[agents$state == "R", ]
+  if(nrow(aS) > 0){
+    points(aS$x, aS$y, pch=15, col="chartreuse3", bg="chartreuse3")
+  }
+  if(nrow(aI) > 0){
+    points(aI$x, aI$y, pch=16, col="firebrick2", bg="firebrick2")
+  }
+  if(nrow(aR) > 0){
+    points(aR$x, aR$y, pch=17, col="goldenrod", bg="goldenrod")
+  }
   graphics.off()
 } # endfor generation
 
