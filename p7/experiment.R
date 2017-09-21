2001b1e1e13a7d9eb24473168f19dca27a8a1510
Alberto Benavides <bena.87@hotmail.com>
2017-09-21 15:59:25 -0500

    Separación por funciones en Práctica 7

--- p7/experiment.R
+++ p7/experiment.R
@@ -2,22 +2,25 @@ experiment <- function(t) {
   curr <- c(
     runif(1, low, high), runif(1, low, high)
   )
-  for (i in 1:tmax) {
+  for (i in 1:maxTime) {
     delta <- runif(1, 0, step)
     left <- curr[1] - delta
     right <- curr[1] + delta
     bottom <- curr[2] - delta
-    top <- curr[2] - delta
-    if (outer(left, bottom, g) > outer(curr[1], curr[2], g)) {
+    top <- curr[2] + delta
+    if(left < low | right > high | bottom < low | top > high){
+      break
+    }
+    if (g(left, bottom) > g(curr[1], curr[2])) {
       curr <- c(left, bottom)
     }
-    if (outer(left, top, g) > outer(curr[1], curr[2], g)) {
+    if (g(left, top) > g(curr[1], curr[2])) {
       curr <- c(left, top)
     }
-    if (outer(right, top, g) > outer(curr[1], curr[2], g)) {
+    if (g(right, top) > g(curr[1], curr[2])) {
       curr <- c(right, top)
     }
-    if (outer(right, bottom, g) > outer(curr[1], curr[2], g)) {
+    if (g(right, bottom) > g(curr[1], curr[2])) {
       curr <- c(right, bottom)
     }
   }
--- p7/main.R
+++ p7/main.R
@@ -1,57 +1,26 @@
-
 library(parallel)
 library(lattice)
+source("g.R")
+source("experiment.R")
 
-g <- function(x, y) {
-  return(((x + 0.5)^4 - 30 * x^2 - 20 * x + (y + 0.5)^4 - 30 * y^2 - 20 * y)/100)
-}
+unlink("img/*.png")
 
-experiment <- function(t) {
-  curr <- c(
-    runif(1, low, high), runif(1, low, high)
-  )
-  for (i in 1:tmax) {
-    delta <- runif(1, 0, step)
-    left <- curr[1] - delta
-    right <- curr[1] + delta
-    bottom <- curr[2] - delta
-    top <- curr[2] + delta
-    if(left < low | right > high | bottom < low | top > high){
-      break
-    }
-    if (g(left, bottom) > g(curr[1], curr[2])) {
-      curr <- c(left, bottom)
-    }
-    if (g(left, top) > g(curr[1], curr[2])) {
-      curr <- c(left, top)
-    }
-    if (g(right, top) > g(curr[1], curr[2])) {
-      curr <- c(right, top)
-    }
-    if (g(right, bottom) > g(curr[1], curr[2])) {
-      curr <- c(right, bottom)
-    }
-  }
-  return(curr)
-}
-
-low <- -5
-high <- 5
-step <- 0.25
+low <- -4
+high <- 4
+step <- 0.2
 repetitions <- 100
-tmax <- 10000
+maxTime <- 100
 
 x <- seq(low, high, step)
 y <- x
 z <- outer(x, y, "g")
-dimnames(z) <- list(x, y)
 d = expand.grid(list(x = x, y = y))
 
 cluster <- makeCluster(detectCores() - 1)
 clusterExport(cluster, "low")
 clusterExport(cluster, "high")
 clusterExport(cluster, "step")
-clusterExport(cluster, "tmax")
+clusterExport(cluster, "maxTime")
 clusterExport(cluster, "g")
 system.time(
   resultados <- parSapply(cluster, 1:repetitions, experiment)
